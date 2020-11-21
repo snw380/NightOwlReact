e0f7673707e3b1c75d5b87d00a8026dd88107d96
snw380 <ty.morgan818@gmail.com>
2020-11-21 08:51:43 -0500

    added front end dev

---
+++ src/components/Members/Members.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Members() {
+    return(
+        <p>Hello world!</p>
+    )
+}
+
+export default Members
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Members from "../components/Members/Members";
+
+function HomePage() {
+    return (
+        <Members/>
+    )
+};
+
+export default HomePage;
